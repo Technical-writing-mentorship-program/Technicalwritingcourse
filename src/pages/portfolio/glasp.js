038c0f1142cfd6003a51f523473402912a753ee6
Aahil13 <onyeanunaprince@gmail.com>
2025-04-23 21:37:11 +0100

    docs: test pushing ability

--- src/pages/portfolio/glasp.js
+++ src/pages/portfolio/glasp.js
@@ -4,7 +4,7 @@ export default function GlaspPortfolio() {
   return (
     <Layout title="Glasp User Guide">
       <main>
-        <h1>Glasp User Guide Assignment</h1>
+        <h1>Glasp User Guide Assignments</h1>
         <p>This portfolio entry showcases a user guide written for Glasp...</p>
         {/* You can include sections like Objective, Outcome, Screenshots, etc. */}
       </main>

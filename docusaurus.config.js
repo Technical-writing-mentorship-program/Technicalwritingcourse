ffcdd5d5cad7c77d5eb72b911e2089e99995b3bd
gloriadasilva <glotejuosho@gmail.com>
2025-05-02 08:34:18 +0100

    I added four markdown courses to the docs folder.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,6 +45,8 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
+          showLastUpdateTime: true,
+          showLastUpdateAuthor: true,
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },

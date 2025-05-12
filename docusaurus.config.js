2d777626d53bffc32ee39f53045227059c40dfff
Wisdom Nwokocha <wise4rmgod@gmail.com>
2025-05-12 21:53:07 +0100

    added new deployment information

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,7 +15,7 @@ const config = {
   favicon: "img/favicon.svg",
 
   // Set the production url of your site here
-  url: "https://your-docusaurus-site.example.com",
+  url: "https://technicalwritingmp.com/",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/",

4199c4ccd8699867cb3e7cb93f5c05ef50a5d127
Spinard <edunrilwan@gmail.com>
2025-12-19 04:38:30 +0100

    test different cohort years

--- src/pages/cohorts/api/index.js
+++ src/pages/cohorts/api/index.js
@@ -43,7 +43,7 @@ export default function CohortsPage() {
 
               <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {menteesByYear.map(({ year, count }) => (
-                  <ListCohortsCard key={year} year={year} count={count} header={header} path={path} />
+                  <ListCohortsCard key={year} year={year} count={count} header={headerText} path={path} />
                 ))}
               </div>
           </div>
